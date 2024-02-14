08de7b58a01ec80f4cd63610b72c64688beaee39
grypto <gryptokaliz.protonmail.com>
2024-02-14 19:41:41 +0200

    update

--- Steam_library_idle/MainWindow.xaml.cs
+++ Steam_library_idle/MainWindow.xaml.cs
@@ -38,17 +38,43 @@ namespace SteamGameLauncher
                         {
                             JArray gamesArray = data.response.games;
                             dynamic[] games = gamesArray.ToObject<dynamic[]>();
+
                             using (StreamWriter batchFile = new StreamWriter("launch_games.bat", false))
                             {
                                 batchFile.WriteLine("@echo off");
-                                foreach (var game in games)
+                                batchFile.WriteLine("color a");
+                                batchFile.WriteLine("title game_loop");
+                                batchFile.WriteLine(":start");
+                                batchFile.WriteLine("cls");
+
+                                int groupSize = 20;
+                                int numGroups = (int)Math.Ceiling((double)games.Length / groupSize);
+
+                                for (int i = 0; i < numGroups; i++)
                                 {
-                                    int appId = game.appid;
-                                    string gameName = game.name;
-                                    string batchContent = $"echo {gameName}\nstart /min steam-idle.exe {appId}\ntimeout 1 > nul\n";
-                                    batchFile.WriteLine(batchContent);
+                                    int startIndex = i * groupSize;
+                                    int endIndex = Math.Min((i + 1) * groupSize, games.Length);
+
+                                    for (int j = startIndex; j < endIndex; j++)
+                                    {
+                                        int appId = games[j].appid;
+                                        string gameName = games[j].name;
+                                        batchFile.WriteLine();
+                                        batchFile.WriteLine($"echo {gameName}");
+                                        batchFile.WriteLine($"start /min steam-idle.exe {appId}");
+                                        batchFile.WriteLine();
+                                    }
+
+                                    batchFile.WriteLine("timeout 3600");
+                                    batchFile.WriteLine("Taskkill /IM steam-idle.exe /F");
+                                    batchFile.WriteLine("cls");
+                                    batchFile.WriteLine("timeout 30");
+                                    batchFile.WriteLine("cls");
                                 }
+
+                                batchFile.WriteLine("goto start");
                             }
+
                             statusTextBlock.Text = "Batch file created (launch_games.bat).";
                             statusTextBlock.Foreground = new SolidColorBrush(Color.FromRgb(39, 174, 96));
                         }
